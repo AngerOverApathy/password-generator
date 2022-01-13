d3a6e53c13571c53a9a77e0139b6ffbbe9373191
AngerOverApathy <angersavestheworld@gmail.com>
2022-01-13 14:20:03 -0800

    added chharacter variables

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,27 @@
 // Assignment code here
 
+var charNumbSymb = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var charLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var charUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// Get references to the #generate element
+function generatePassword () {
+console.log(upperCaseChar)
+// pprompt user for password criteria
+
+//length of pass 8-28, lowercase, uppercase, special characters
+var confirmLength = (prompt("How many characters in your password? Choose between 8-28."));
+//input validation
+
+//generate password
+
+
+
+//display pass
+  return "Password will go here!"
+};
+
+
+// Get references to the #generate element targets generate btn
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -13,5 +33,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// when button is cliked, it will call writePass function
 generateBtn.addEventListener("click", writePassword);
